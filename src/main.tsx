025e4539a285e2f6add85c356423fb453efc3642
nijigatya <yotpfx@gmail.com>
2024-11-17 21:22:14 +0900

    Initial commit: PWA setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,14 @@ import App from './App.tsx';
 import './index.css';
 
 const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
+  onNeedRefresh() {
+    if (confirm('新しいバージョンが利用可能です。更新しますか？')) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    alert('アプリがオフラインで使用できるようになりました！');
+  },
 })
 
 createRoot(document.getElementById('root')!).render(
